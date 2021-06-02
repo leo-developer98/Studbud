b2d8b4725bcf6c455f174497ef1cbdf56f452484
Leo Park <ssangminleo@gmail.com>
2021-06-03 01:41:27 +0900

    dictionary working (words API)

--- dist/index.8a5bc16d.js
+++ dist/index.8a5bc16d.js
@@ -10955,78 +10955,139 @@ var define;
 // SOFTWARE.
 
 
-// Specifies a function to execute when the DOM is fully loaded.
-$(document).ready(function(){
-    // adds a submit listened to our <form> element
-    $("#main-form").submit(async (event) => {
-        // prevents the page from reloading on subject
-        event.preventDefault();
-        // adds the text 'Loading...' to our word 
-        // data container for UX purposes
-        $('#word-info').html('Loading...');
-        // collects the value in the input form element
-        // by the id on the element
-        const word = $("#word-input").val();
-        // creates a variable that represents our
-        // word info container
-        let wordInfoList = document.querySelector('#word-info');
-        try {
-            // asynchronously calls our custome function
-            const data = await (await fetch(`http://localhost:9000/.netlify/functions/getWord?word=${word}`, { mode: 'cors'})).json();
-            // logs no results if word data is not found
-            if (data.length < 1) {
-                return wordInfoList.appendChild(document.createTextNode('No results matched.'));
-            }
-            // clears the word container if it had
-            // previous data
-            $('#word-info').empty();
-            data.map(val => {
-                // creates parent li element
-                const li = document.createElement('li');
-                li.classList.add('my-4', 'p-4', 'list-item');
-                // loops over the values for each definition
-                val.map(property => {
-                    if (property.label === 'definition') {
-                        // creates new heading-3 element
-                        const def = document.createElement('h3');
-                        // adds text to the element
-                        def.innerText = property.value;
-                        // appends class value for styling
-                        def.classList.add(['definition']);
-                        // adds the element to our list item
-                        li.appendChild(def);
-                    } else if (property.isString) {
-                        const partOfSpeech = document.createElement('small');
-                        partOfSpeech.innerText = property.value;
-                        partOfSpeech.classList.add('lead','font-italic');
-                        li.appendChild(partOfSpeech);
-                    } else {
-                        const characteristic = document.createElement('dl');
-                        characteristic.className = 'row';
-                        const label = document.createElement('dt');
-                        label.innerText = property.label;
-                        label.className = 'col-sm-3';
-                        const value = document.createElement('dd');
-                        value.innerText = property.value.join(', ');
-                        value.className = 'col-sm-9';
-                        characteristic.appendChild(label);
-                        characteristic.appendChild(value);
-                        li.appendChild(characteristic);
-                    }
-                })
-                // appends the list item fully formed to
-                // the word data container
-                wordInfoList.appendChild(li);
-            })
-        } catch (e) {
-            // logs the error if one exists
-            console.log(e);
-            // displays message to user if there is an error
-            $('#word-info').html('There was an error fetching the word data');
+
+// // Specifies a function to execute when the DOM is fully loaded.
+// $(document).ready(function(){
+//     // adds a submit listened to our <form> element
+//     $("form").submit(async (event) => {
+//         // prevents the page from reloading on subject
+//         event.preventDefault();
+//         // adds the text 'Loading...' to our word 
+//         // data container for UX purposes
+//         $('#word-info').html('Loading...');
+//         // collects the value in the input form element
+//         // by the id on the element
+//         const word = $("#word-input").val();
+//         // creates a variable that represents our
+//         // word info container
+//         let wordInfoList = document.querySelector('#word-info');
+//         try {
+//             // asynchronously calls our custome function
+//             const data = await (await fetch(`http://localhost:9000/.netlify/functions/getWord?word=${word}`, { mode: 'cors'})).json();
+//             // logs no results if word data is not found
+//             if (data.length < 1) {
+//                 return wordInfoList.appendChild(document.createTextNode('No results matched.'));
+//             }
+//             // clears the word container if it had
+//             // previous data
+//             $('#word-info').empty();
+//             data.map(val => {
+//                 // creates parent li element
+//                 const li = document.createElement('li');
+//                 li.classList.add('my-4', 'p-4', 'list-item');
+//                 // loops over the values for each definition
+//                 val.map(property => {
+//                     if (property.label === 'definition') {
+//                         // creates new heading-3 element
+//                         const def = document.createElement('h3');
+//                         // adds text to the element
+//                         def.innerText = property.value;
+//                         // appends class value for styling
+//                         def.classList.add(['definition']);
+//                         // adds the element to our list item
+//                         li.appendChild(def);
+//                     } else if (property.isString) {
+//                         const partOfSpeech = document.createElement('small');
+//                         partOfSpeech.innerText = property.value;
+//                         partOfSpeech.classList.add('lead','font-italic');
+//                         li.appendChild(partOfSpeech);
+//                     } else {
+//                         const characteristic = document.createElement('dl');
+//                         characteristic.className = 'row';
+//                         const label = document.createElement('dt');
+//                         label.innerText = property.label;
+//                         label.className = 'col-sm-3';
+//                         const value = document.createElement('dd');
+//                         value.innerText = property.value.join(', ');
+//                         value.className = 'col-sm-9';
+//                         characteristic.appendChild(label);
+//                         characteristic.appendChild(value);
+//                         li.appendChild(characteristic);
+//                     }
+//                 })
+//                 // appends the list item fully formed to
+//                 // the word data container
+//                 wordInfoList.appendChild(li);
+//             })
+//         } catch (e) {
+//             // logs the error if one exists
+//             console.log(e);
+//             // displays message to user if there is an error
+//             $('#word-info').html('There was an error fetching the word data');
+//         }
+//     });
+// });
+
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://wordsapiv1.p.rapidapi.com/words/example",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "1a22e3887bmshe53e197fd1cbcacp187fc3jsnce0fb7bfef1a",
+// 		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+// 	}
+// };
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+const searchBtn = document.getElementById("dicSearchBtn");
+const searchInput = document.getElementById("searchBox");
+
+searchBtn.addEventListener("click", function() {
+    let word = searchInput.value;
+    // var request = new XMLHttpRequest();
+
+    // request.open('GET', `https://api.dictionaryapi.dev/api/v2/entries/en_UK/${word}`);
+
+    // request.onload = function() {
+    //     console.log(this.response);
+    // }
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://wordsapiv1.p.rapidapi.com/words/${word}`,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "1a22e3887bmshe53e197fd1cbcacp187fc3jsnce0fb7bfef1a",
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
         }
-        // window.location.reload();
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        let resultLength = response.results.length;
+        $("#word").html(response.word);
+        $("#pronunciation").html(response.pronunciation.all);
+        for (let i = 0; i < resultLength; i++) {
+            let result = response.results[i];
+            let box = $("<div></div>");
+            let def = $("<p></p>").text(result.definition);
+            let mode = $("<p></p>").text(result.partOfSpeech);
+            $("#definitions").append(box);
+            box.append(mode);
+            box.append(def);
+        }
     });
-});
+
+    $.ajax(settings).fail(function() {
+        console.log("No word found");
+    })
+})
+
 },{}],"6m8Cd":[function(require,module,exports) {
 const musicPlayer = document.getElementById("musicPlayer");
 
--- functions/getWord.js
+++ functions/getWord.js
@@ -1,58 +1,57 @@
 const axios = require('axios')
-
-
+// 'event' and 'context' are automatically passed to the function
+// event contains the query parameters that we will be passing with the API call
 export async function handler(event, context) {
-    //   extract the word query parameter from the HTTP request
-      const word = event.queryStringParameters.word || "automobile";
-      try {
-        // send request to the WordsAPI
-        const response = await axios({
-          "method":"GET",
-          "url":`https://wordsapiv1.p.rapidapi.com/words/${word}`,
-          "headers":{
-          "content-type":"application/octet-stream",
-          "x-rapidapi-host":"wordsapiv1.p.rapidapi.com",
-          "x-rapidapi-key":"1a22e3887bmshe53e197fd1cbcacp187fc3jsnce0fb7bfef1a"
-          }
-        })
-        // START OF NEW CODE
-        // create new array to push data to
-        let results = [];
-        response.data.results.map(def => {
-          let definitionArray = [];
-          // creates array of keys in object
-          const keys = Object.keys(def);
-          keys.map(key => {
-            // builds regex that looks for capital letters
-            // The response parameters are in camelCase, so we need to ID
-            // the capital letters and add spaces instead so the 
-            // front end can easily display the parameter labels
-            const regex = /(?=[A-Z])/;
-            // creates presentable label
-            const label = key.split(regex).join(' ').toLowerCase();
-            // grabs the value for the parameter from the
-            // definition object in response
-            const value = def[key];
-            // constructs new object to send to frontend
-            let newObj = {
-              label,
-              value,
-              isString: typeof value === 'string' ? true : false
-            };
-            definitionArray.push(newObj)
-          })
-          results.push(definitionArray)
-        })
-        // END NEW CODE
-        return {
-          statusCode: 200,
-          body: JSON.stringify(response.data),  // modified
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          }
-        }
-      } catch (err) {
-        console.log(err)
-        return { statusCode: 500, body: err.toString() }
+//   extract the word query parameter from the HTTP request
+  const word = event.queryStringParameters.word || "automobile";
+  try {
+    // send request to the WordsAPI
+    const response = await axios({
+      "method":"GET",
+      "url":`https://wordsapiv1.p.rapidapi.com/words/${word}`,
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"wordsapiv1.p.rapidapi.com",
+      "x-rapidapi-key":"02df4fb51bmsh702ed367223bfccp13c6d8jsnf367d77b2d56"
+      }
+    })
+    // START OF NEW CODE
+    // create new array to push data to
+    let results = [];
+    response.data.results.map(def => {
+      let definitionArray = [];
+      // creates array of keys in object
+      const keys = Object.keys(def);
+      keys.map(key => {
+        // builds regex that looks for capital letters
+        // The response parameters are in camelCase, so we need to ID
+        // the capital letters and add spaces instead so the 
+        // front end can easily display the parameter labels
+        const regex = /(?=[A-Z])/;
+        // creates presentable label
+        const label = key.split(regex).join(' ').toLowerCase();
+        // grabs the value for the parameter from the
+        // definition object in response
+        const value = def[key];
+        // constructs new object to send to frontend
+        let newObj = {
+          label,
+          value,
+          isString: typeof value === 'string' ? true : false
+        };
+        definitionArray.push(newObj)
+      })
+      results.push(definitionArray)
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
       }
     }
+  } catch (err) {
+    console.log(err)
+    return { statusCode: 500, body: err.toString() }
+  }
+}
